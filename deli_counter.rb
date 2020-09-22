91d9fd92050a82bc4bf7f590d393295f9b390596
Ryan K Burns <ryankburns@gmail.com>
2020-09-22 23:01:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,9 @@ def line(katz_deli)
 if katz_deli.empty?
   puts "The line is currently empty."
 else
-  puts "The line is currently: " + katz_deli.join
+  messege = "The line is currently: "
+katz_deli.each_with_index do |name, index|  
+  messege += "#{index.to_i + 1}. #{name}"
 end
 end
 
@@ -17,7 +19,7 @@ end
 def now_serving(katz_deli)
 if katz_deli.empty?
   puts "There is nobody waiting to be served!"
-else
+else 
   puts "Currently serving #{katz_deli.shift}."
 end
 end
