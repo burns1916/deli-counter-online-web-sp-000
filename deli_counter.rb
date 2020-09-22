9387c344fd9255733ade5560143c7bf0351960df
Ryan K Burns <ryankburns@gmail.com>
2020-09-22 22:28:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,14 @@ katz_deli = []
 def line(katz_deli)
 if katz_deli.length == 0
   puts "The line is currently empty."
+else
+  puts "The line is currently: "
 end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number" katz_deli.length " in line"
+end
+
+
