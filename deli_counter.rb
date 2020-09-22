73c01e610ab7ea82ea7007d42439b1900984de6c
Ryan K Burns <ryankburns@gmail.com>
2020-09-22 23:02:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,11 @@ def line(katz_deli)
 if katz_deli.empty?
   puts "The line is currently empty."
 else
-  messege = "The line is currently: "
+  current_line = "The line is currently: "
 katz_deli.each_with_index do |name, index|
-  messege += "#{index.to_i + 1}. #{name}"
+  current_line += "#{index.to_i + 1}. #{name}"
 end
+puts "#{current_line}"
 end
 
 def take_a_number(katz_deli, name)
